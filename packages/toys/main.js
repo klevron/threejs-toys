473574c05a6fa2a7d3883926f88f2bd558d024d9
Kevin Levron <kevin.levron@gmail.com>
2022-05-03 18:47:11 +0200

    main test

--- packages/toys/main.js
+++ packages/toys/main.js
@@ -1,5 +1,17 @@
 import './style.css'
 
-import background from './src/cursors/neon-cursor'
+// import background from './src/cursors/neon-cursor'
+import background from './src/backgrounds/noisy-lines'
 
-background({ el: document.getElementById('app') })
+const bg = background({ el: document.getElementById('app') })
+
+document.body.addEventListener('click', () => {
+  bg.config.colors = [Math.random() * 0xffffff, Math.random() * 0xffffff]
+  bg.config.minStroke = Math.random() * 2
+  bg.config.maxStroke = bg.config.minStroke + Math.random() * 5
+  bg.drawTexture()
+
+  bg.config.timeCoef = 0.000025 + Math.random() * 0.001
+  bg.uniforms.uCoordScale.value = 0.5 + Math.random() * 4.5
+  bg.uniforms.uDisplacementScale.value = 0.00025 + Math.random() * 0.01
+})
