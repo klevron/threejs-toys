7b8b2ef457f7ec0d202eecb80e800361766f2ab1
Kevin Levron <kevin.levron@gmail.com>
2022-05-23 23:55:36 +0200

    export butteflies

--- packages/toys/src/export.js
+++ packages/toys/src/export.js
@@ -1,3 +1,4 @@
 export { default as neonCursor } from './cursors/neon/index'
 export { default as particlesCursor } from './cursors/particles/index'
 export { default as noisyLinesBackground } from './backgrounds/noisy-lines/index'
+export { default as butterfliesBackground } from './backgrounds/butterflies/index'
